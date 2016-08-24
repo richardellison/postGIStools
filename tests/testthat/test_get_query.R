d1756649a0fc2d84f46d4e873e9368d3c07ffd12
Philippe Marchand <pmarchand@rstudio01.research.sesync.org>
2016-08-24 14:44:25 -0400

    Additional tests for get_postgis_query

--- tests/testthat/test_get_query.R
+++ tests/testthat/test_get_query.R
@@ -49,8 +49,45 @@ test_that("get_postgis_query correctly imports geometry", {
     expect_equal(proj4string(qry), proj_wgs84)
     expect_equal(over(pts, qry)$name, c("Tanzania", "Kenya"))
     # Importing just geometry should result in zero-column @data
-    expect_equal(length(qry2@polygons), 2)
+    expect_length(qry2@polygons, 2)
     expect_equal(dim(qry2@data), c(2, 0))
+})
+
+
+test_that("get_postgis_query works with SELECT * (wildcard)", {
+    if (is.null(con)) skip("PostgreSQL connection unavailable")
+    qry <- get_postgis_query(con,
+                             "SELECT * FROM country WHERE iso2 IN ('KE', 'TZ')",
+                             geom_name = "geom", hstore_name = "translations")
+    pts <- SpatialPoints(cbind(c(35.7, 36.8), c(-6.2, -1.3)),
+                         proj4string = CRS(proj4string(qry)))
+    # Check overlay with known points
+    expect_equal(over(pts, qry)$name, c("Tanzania", "Kenya"))
+    # Check hstore data
+    expect_equal(qry$translations[1] %->% "es", "Kenia")
+})
+
+
+test_that("get_postgis_query correctly imports points from ST_Centroid", {
+    if (is.null(con)) skip("PostgreSQL connection unavailable")
+    qry <- get_postgis_query(con,
+        "SELECT ST_Centroid(geom) centr FROM country WHERE iso2 IN ('KE', 'TZ')",
+        geom_name = "centr")
+    # Verify class and x coord. of first point (within 0.001)
+    expect_is(qry, "SpatialPointsDataFrame")
+    expect_lt(abs(qry@coords[1, 1] - 37.8247), 0.001)
+})
+
+
+test_that("get_postgis_query correctly imports lines from ST_Boundary", {
+    if (is.null(con)) skip("PostgreSQL connection unavailable")
+    qry <- get_postgis_query(con,
+        "SELECT ST_Boundary(geom) bound FROM country WHERE iso2 IN ('KE', 'TZ')",
+        geom_name = "bound")
+    # Verify class and number of lines
+    expect_is(qry, "SpatialLinesDataFrame")
+    expect_length(qry@lines[[1]]@Lines, 2)
+    expect_length(qry@lines[[2]]@Lines, 4)
 })
 
 
